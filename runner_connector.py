a1f6de0b0030f231ecad7c043eb67704eb6afbee
root <root@splunksoar>
2024-06-03 09:47:07 -0700

    Release notes for version 1.0.10

--- runner_connector.py
+++ runner_connector.py
@@ -1,6 +1,6 @@
 # File: runner_connector.py
 #
-# Copyright (c) Mhike, 2024
+# Copyright (c) Mhike, 2022
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
